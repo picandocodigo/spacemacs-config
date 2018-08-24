509e53376ddea084d50eb4f8a83a2241627dc1fe
Fernando Briano <fernando@picandocodigo.net>
2018-08-24 15:46:29 +0100

    Add more Go stuff

--- personal-config-layer/config.el
+++ personal-config-layer/config.el
@@ -23,7 +23,9 @@
 (setq helm-completion-in-region-fuzzy-match t)
 
 ;; Go
+(setq go-format-before-save t)
 (setq gofmt-command "goimports")
+(setq go-tab-width 4)
 
 ;; Always start a server:
 (require 'server)
