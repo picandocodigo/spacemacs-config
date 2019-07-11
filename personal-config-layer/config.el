41884ae96d8ffefd67a9c57b8e6fbfd4f732b36b
Fernando Briano <fernando@picandocodigo.net>
2019-07-11 14:00:14 +0100

    Migrate to a personal dotfile

--- personal-config-layer/config.el
+++ personal-config-layer/config.el
@@ -1,21 +1,18 @@
-;;; Set emacs mode as default
-(setq dotspacemacs-editing-style 'emacs)
-(setq dotspacemacs-mode-line-theme 'spacemacs)
+(add-hook 'magit-mode-hook 'visual-line-mode)
 
 ;;; ORG MODE
-(add-hook 'magit-mode-hook 'visual-line-mode)
 ;; Add hook for doing this on every org file:
 (add-hook 'org-mode-hook 'org-html-export-on-save)
 ;; Enable `org-indent-mode' by default
 (setq org-startup-indented t)
-
 ;; Use visual line mode to wrap text in screen space:
 (add-hook 'org-mode-hook 'visual-line-mode)
 
 ;;; THEME
-(spacemacs/load-theme 'deeper-blue)
+(spacemacs/load-theme 'doom-opera)
 (setq auto-window-vscroll nil)
 
+;;;; Ruby
 (setq ruby-insert-encoding-magic-comment nil)
 (setq enh-ruby-add-encoding-comment-on-save nil)
 
