7aa29d3afb835ad41b876ad29a6279e3c5c2e785
Fernando Briano <fernando@picandocodigo.net>
2020-07-20 14:40:42 +0100

    Adds archived state for org items

--- personal-config-layer/config.el
+++ personal-config-layer/config.el
@@ -9,7 +9,7 @@
 (add-hook 'org-mode-hook 'visual-line-mode)
 (setq org-todo-keywords
       '(
-        (sequence "TODO(t)" "MAYBE(m)" "IN PROGRESS(p)" "|" "✅ DONE(d)")
+        (sequence "TODO(t)" "MAYBE(m)" "IN PROGRESS(p)" "🗄 ARCHIVED(a)" "|" "✅ DONE(d)")
         (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
         )
       )
